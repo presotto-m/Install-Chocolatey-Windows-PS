697ca50bd46ca43d31f3ee24f0cd08d5a5fc460d
Matheus Presotto <83012707+presotto-m@users.noreply.github.com>
2023-02-13 04:29:07 -0800

    Create choco.ps1

---
+++ choco.ps1
@@ -0,0 +1,7 @@
+Get-ExecutionPolicy
+
+Set-ExecutionPolicy AllSigned
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco search google chrome
